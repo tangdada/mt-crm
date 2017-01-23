175c47abea83b7748eaba25c025a49c097084e7e
xqtang <txq@mytijian.com>
2017-01-23 16:41:02 +0800

    first commit

---
+++ client/app.js
@@ -0,0 +1,17 @@
+import Promise from 'promise-polyfill'
+window.Promise = window.Promise || Promise
+
+import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
+import App from './components/App'
+import router from './router'
+import store from './store'
+sync(store, router)
+
+const app = new Vue({
+  router,
+  store,
+  ...App
+})
+
+app.$mount('#app');
